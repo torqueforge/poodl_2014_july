a52629cfedfe071e04a10d3a4cdd695269489872
Megan Folsom <mfolsom@gmail.com>
2014-07-03 15:42:51 +0100

    Start open closed principle

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,28 +1,59 @@
 # Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
+# puts instructions(__FILE__)
 
 class Bottles
+  def verse(bottle_number)
+    current_bottle_number = bottle_number
+    next_bottle_number = next_bottle_number(current_bottle_number)
+     "#{bottles_left(current_bottle_number)} #{bottle(current_bottle_number)} of beer on the wall, #{bottles_left(current_bottle_number)} #{bottle(current_bottle_number)} of beer.\n".capitalize+
+     "#{next_move(current_bottle_number)}, #{bottles_left(next_bottle_number)} #{bottle(next_bottle_number)} of beer on the wall.\n"
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).map { |n| verse(n) }.join("\n")
+  end
+
   def song
     verses(99,0)
   end
- 
-  def verses(upper, lower)
-    upper.downto(lower).map { |bottle_number| verse(bottle_number) }.join("\n")
-  end
- 
-  def verse(bottle_number)
-    current_bottle_number = bottle_number
-    next_bottle_number = current_bottle_number - 1
-    case current_bottle_number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n" 
+
+  def bottle(bottle_number)
+    if bottle_number == 1 
+      "bottle"
     else
-      "#{current_bottle_number} bottles of beer on the wall, #{current_bottle_number} bottles of beer.\nTake one down and pass it around, #{next_bottle_number} bottles of beer on the wall.\n"
+      "bottles"
     end
   end
 
+  def pronoun(bottle_number)
+    if bottle_number == 1 
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def bottles_left(bottle_number)
+    if bottle_number == 0 
+      "no more"
+    else
+      bottle_number
+    end
+  end
+
+  def next_bottle_number(bottle_number)
+    if bottle_number == 0
+      99
+    else
+      bottle_number - 1
+    end
+  end
+
+  def next_move(bottle_number)
+    if bottle_number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(bottle_number)} down and pass it around"
+    end
+  end
 end
